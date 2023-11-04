bfd5822f3e04394e84e587becb7e75443f53b856
Gabe <gabriel.velasquez0116@gmail.com>
2023-11-04 16:34:53 -0700

    still working on projects and how it is displayed

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,34 +1,25 @@
 const projects=[
     {
-        title:'',
-        body:'',
-        site:'',
-        screenshot: ''
+        title:'Infinite Photo Reel',
+        body:"This project was built so I could get familiar with calling APIs in an application and seemlessly build with a user in mind.",
+        site:'https://gabe-velasquez.github.io/Infinite-Photo-Reel-Project/',
+        source:'https://github.com/Gabe-Velasquez/Infinite-Photo-Reel-Project',
+        screenshot: '../src/assets/photoReel.png'
     },
     {
-        title:'',
-        body:'',
-        site:'',
-        screenshot: ''
+        title:'Starby Cup Corner',
+        body:"The purpose of this web application is to allow users to create an account, search for Starbucks™ cups that other user's are selling and also allow users to post their own cups. We used a variety of technology from HTML-CSS-JS to Express and Handlebars.",
+        site:'https://starbycupcorner-e77b9a9ffe86.herokuapp.com/',
+        source:'https://github.com/shinyuta/Project-2-UofMN',
+        screenshot: '../src/assets/SCC.png'
     },
     {
-        title:'',
-        body:'',
-        site:'',
-        screenshot: ''
+        title:'Face Detection Application',
+        body:"I have created a cutting-edge application that empowers users to seamlessly upload images to an application platform interface (API) and receive precise facial coordinate results in return. This user-friendly interface not only swiftly returns the image with marked facial coordinates but also includes robust security features through integrated sign-in and registration functions. This project was brought to life through the utilization of a powerful technology stack, featuring React for the frontend, Node for the backend, and Postgres for data management. To ensure seamless deployment and hosting, I harnessed the capabilities of Render.com, further enhancing the application's reliability and performance. I'm eager to discuss this project and explore how my skills can contribute to your team's success.",
+        site:'https://face-recognition-front-end-ws1v.onrender.com/',
+        source:'https://github.com/Gabe-Velasquez/FaceRecognitionApp',
+        screenshot: '../src/assets/FaceDetect.png'
     },
-    {
-        title:'',
-        body:'',
-        site:'',
-        screenshot: ''
-    },
-    {
-        title:'',
-        body:'',
-        site:'',
-        screenshot: ''
-    }
 ]
 
 export default projects;
