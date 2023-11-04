ad5caae66e6ac9df5a6c4e5670493adac09f3238
Gabe <gabriel.velasquez0116@gmail.com>
2023-11-04 12:44:49 -0700

    made jsx for about and portfolio

---
+++ src/Pages/About.jsx
@@ -0,0 +1,12 @@
+export default function About(){
+    return (
+        <div>
+            <h2>About Me</h2>
+            <img src=''/>
+            <p>Hello, I&#39m Gabe, a passionate web developer who recently relocated to the vibrant tech hub of Seattle, originally hailing from the scenic landscapes of Colorado. I thrive on the boundless opportunities that technology offers to exercise creativity while solving complex problems. Collaborating with like-minded individuals to overcome challenges is a driving force behind my work.</p>
+            <p>My journey in tech has been enriched by my experience in team management and a relentless pursuit of innovation in processes. My primary objective in any professional setting is process enhancement and equipping my team with the tools they need to thrive. To me, successful teamwork hinges on three pivotal factors: communication, documentation, and execution.</p>
+            <p>Beyond the confines of code and development, you&#39ll find me exploring the great outdoors with my family. Whether it&#39s a sunny day at the park with our dogs or cheering for our favorite hockey team, I cherish these moments of recreation and connection. I firmly believe in the importance of continuous learning, a necessity in a field marked by ever-evolving technologies and industry dynamics.</p>
+            <p>In a fast-paced tech world, I am your dedicated problem solver, collaborator, and process improver, committed to helping my team succeed. I&#39m excited to explore opportunities that align with my values and skills, and I look forward to contributing my expertise to your organization&#39s success. Let&#39s connect and discuss how I can be a valuable asset to your team.</p>
+        </div>
+    )
+}
---
+++ src/Pages/Portfolio.jsx
@@ -0,0 +1,10 @@
+//TODO: Create helper for projects
+
+export default function Portfolio(){
+    return(
+        <div>
+            <h2>Portfolio</h2>
+            {/* Project map will go here  */}
+        </div>
+    )
+}
