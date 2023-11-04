858047b4ec5f071e7d685ffb95a8ceb93d4b4053
Gabe <gabriel.velasquez0116@gmail.com>
2023-11-04 14:17:53 -0700

    built header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import Nav from "./Nav";
+
+export default function Header(){
+    return(
+        <header>
+            <h1>Gabe Velasquez</h1>
+            <p>Software Developer</p>
+            <Nav />
+        </header>
+    )
+}
