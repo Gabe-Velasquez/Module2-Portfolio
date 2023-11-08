d96b0595f9abdf3f59c08db1bdb91c8dd4c998a9
Gabe <gabriel.velasquez0116@gmail.com>
2023-11-07 18:39:19 -0800

    styled navbar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,9 +2,11 @@ import Nav from "./Nav";
 
 export default function Header(){
     return(
-        <header>
-            <h1>Gabe Velasquez</h1>
-            <p>Software Developer</p>
+        <header className="header-info">
+            <div className="header-wrapper">
+                <h1>Gabe Velasquez</h1>
+                <p>Software Developer</p>
+            </div>
             <Nav />
         </header>
     )
