051461c3a3ea34c8728ea2e68bfa2133aef943fd
Gabe <gabriel.velasquez0116@gmail.com>
2023-11-08 16:31:59 -0800

    adding styling for resume before class

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -1,26 +1,41 @@
 export default function Resume(){
     return(
-        <div>
-            <div>
-                <h2>Resume</h2>
-                <span>
-                    Download Resume{' '}
-                    <a 
-                        href="../src/assets/GabeVelasquez_Resume.pdf"
-                        download="{GabeVelasquez_Resume.pdf}">
-                            here!
-                    </a>
-                </span>
+        <div className="res-main">
+            <div className="res-top">
+                <div >
+                    <h2>Resume</h2>
+                    <span>
+                        Download Resume{' '}
+                        <a 
+                            href="../src/assets/GabeVelasquez_Resume.pdf"
+                            download="{GabeVelasquez_Resume.pdf}">
+                                here!
+                        </a>
+                    </span>
+                </div>
+
+                <div>
+                    <h3>Certifications</h3>
+                    <span>
+                        <a href="../src/assets/certificate-of-completion-for-complete-web-developer-in-2020-zero-to-mastery.pdf" target="_blank">Zero to Mastery Web Developer</a>
+                    </span>
+                </div>
             </div>
 
-            <div>
-                <h3>Certifications</h3>
-                <span>
-                    <a href="../src/assets/certificate-of-completion-for-complete-web-developer-in-2020-zero-to-mastery.pdf" target="_blank">Zero to Mastery Web Developer</a>
-                </span>
-            </div>
-
-            <div>
+            <div className="skills">
+                <div>
+                <h3>Front End Skills</h3>
+                <ul>
+                    <li>React</li>
+                    <li>JavaScript</li>
+                    <li>HTML5</li>
+                    <li>CSS3</li>
+                    <li>CSS Frameworks like Bootstrap, Bulma and Tachyons</li>
+                    <li>jQuery</li>
+                    <li>Git</li>
+                </ul>
+                </div>
+                <div>
                 <h3>Backend Skills</h3>
                 <ul>
                     <li>Node</li>
@@ -35,16 +50,7 @@ export default function Resume(){
                     <li>Apollo Server</li>
                     <li>Sequelize</li>
                 </ul>
-                <h3>Front End Skills</h3>
-                <ul>
-                    <li>React</li>
-                    <li>JavaScript</li>
-                    <li>HTML5</li>
-                    <li>CSS3</li>
-                    <li>CSS Frameworks like Bootstrap, Bulma and Tachyons</li>
-                    <li>jQuery</li>
-                    <li>Git</li>
-                </ul>
+                </div>
             </div>
         </div>
     )
