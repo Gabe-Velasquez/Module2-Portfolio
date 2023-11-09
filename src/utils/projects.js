24c30bf4cc505aabd090635be351353fe76b7635
Gabe <gabriel.velasquez0116@gmail.com>
2023-11-09 12:46:01 -0800

    more file changes to paths

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -4,42 +4,42 @@ const projects=[
         body:"This project was built so I could get familiar with calling APIs in an application and seemlessly build with a user in mind.",
         site:'https://gabe-velasquez.github.io/Infinite-Photo-Reel-Project/',
         source:'https://github.com/Gabe-Velasquez/Infinite-Photo-Reel-Project',
-        screenshot: '/src/assets/images/photoReel.png'
+        screenshot: '../images/photoReel.png'
     },
     {
         title:'Starby Cup Corner',
         body:"The purpose of this web application is to allow users to create an account, search for Starbucks™ cups that other user's are selling and also allow users to post their own cups. We used a variety of technology from HTML-CSS-JS to Express and Handlebars.",
         site:'https://starbycupcorner-e77b9a9ffe86.herokuapp.com/',
         source:'https://github.com/shinyuta/Project-2-UofMN',
-        screenshot: './src/assets/images/SCC.png'
+        screenshot: '../images/SCC.png'
     },
     {
         title:'Face Detection Application',
         body:"I have created a cutting-edge application that empowers users to seamlessly upload images to an application platform interface (API) and receive precise facial coordinate results in return. This user-friendly interface not only swiftly returns the image with marked facial coordinates but also includes robust security features through integrated sign-in and registration functions. This project was brought to life through the utilization of a powerful technology stack, featuring React for the frontend, Node for the backend, and Postgres for data management. To ensure seamless deployment and hosting, I harnessed the capabilities of Render.com, further enhancing the application's reliability and performance. I'm eager to discuss this project and explore how my skills can contribute to your team's success.",
         site:'https://face-recognition-front-end-ws1v.onrender.com/',
         source:'https://github.com/Gabe-Velasquez/FaceRecognitionApp',
-        screenshot: './src/assets/images/FaceDetect.jpg'
+        screenshot: '../images/FaceDetect.jpg'
     },
     {
         title: 'Whose Hotter',
         body: 'The first group project I worked in my coding bootcamp where we built an application that compared two contries in regards to their weather, main language spoken and currency to help with those individuals that want to travel on short notice.',
         site: 'https://gabe-velasquez.github.io/Whose_Hotter/',
         source: 'https://github.com/Gabe-Velasquez/Whose_Hotter',
-        screenshot: '../src/assets/images/WhoseHotter.png'
+        screenshot: '../images/WhoseHotter.png'
     },
     {
         title: 'Seattle Quiz',
         body: 'This is a quiz I made in class when I was first taught JavaScript. It taught me how to access and use local storage for an application and how to have a better idea of conditional programming in general. ',
         site: 'https://gabe-velasquez.github.io/Quiz-Project/',
         source: 'https://github.com/Gabe-Velasquez/Quiz-Project',
-        screenshot: '../src/assets/images/Quiz.png'
+        screenshot: '../images/Quiz.png'
     },
     {
         title: 'Password Generator',
         body: 'A password generator I made in my coding bootcamp when we were first learning about javascript. Had starter code to help but had to write the logic of the application. Something I can come back to and see where I first started my programming journey.',
         site: 'https://gabe-velasquez.github.io/Password-Generator/',
         source: 'https://github.com/Gabe-Velasquez/Password-Generator',
-        screenshot: '../../src/assets/images/password-gen.png'
+        screenshot: '../images/password-gen.png'
     }
 ]
 
